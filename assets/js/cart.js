1bcdb811e475a1bb907a04a373c653473096221a
kdays1 <caroodzg@gmail.com>
2022-08-07 22:27:56 -0500

    delete items from cart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -13,6 +13,11 @@ function checkCartStorage () {
         for (i=0; i<intheCart.length; i++) {
             order = 'Chamarra tamaño: ' + intheCart[i].talla + ' color: ' + intheCart[i].color + ' con la frase: ' + intheCart[i].frase + '. Tu total es de $' + intheCart[i].precio;
             contenedor = document.getElementById("thisCart");
+            contenedor.setAttribute("id","contenedor"+i);
+            numeracion = document.createElement("span");
+            num = document.createTextNode(i+") ");
+            numeracion.appendChild(num);
+            contenedor.appendChild(numeracion);
             dentroContenedor = document.createElement("div");
             contenedor.appendChild(dentroContenedor);
             contenido = document.createTextNode(order);
@@ -22,6 +27,10 @@ function checkCartStorage () {
             console.log(order);
             suma[i] = parseInt(intheCart[i].precio, 10);
             total = total + suma[i];
+            deleteItem = document.createElement("button");
+            deleteItem.setAttribute("id","btn" + i);
+            contenedor.appendChild(deleteItem);
+            deleteItem.addEventListener("click",deleteItem())
         }
         // document.getElementById("thisCart").innerHTML = order;
         totalElement = document.createElement("div");
@@ -31,6 +40,10 @@ function checkCartStorage () {
     }
 }
 checkCartStorage ();
+
+function deleteItem () {
+
+}
 // alert("Te muestro tu pedido: chamarra de talla " + pedidos[conteo_pedidos].chamarra.talla + " de color " + pedidos[conteo_pedidos].chamarra.color + " con " + pedidos[conteo_pedidos].chamarra.bordados + " bordado(s) a nombre de " + pedidos[conteo_pedidos].nombrede + ". Tu total es de: $" + pedidos[conteo_pedidos].chamarra.precio + "MXN");
 // alert("¡Gracias por tu pedido (:!");
 // console.log('Ultimo Pedido => número: ' + conteo_pedidos + ' a nombre de: ' + pedidos[conteo_pedidos].nombrede);
